951ddf485425fdb079f557ece498c5d78cec0de7
marcinzygan <soul@o2.pl>
2022-12-04 11:20:00 +0100

    added general layout for the application

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,25 @@
-import NewsPage from "./news";
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "first meetup",
+    image:
+      "https://images.unsplash.com/photo-1646905065090-abeff1ccf975?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+    address: "Portishead, UK",
+    description: "First meetup",
+  },
+  {
+    id: "m2",
+    title: "second meetup",
+    image:
+      "https://images.unsplash.com/photo-1655283658746-7402b3195f88?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
+    address: "Portland, UK",
+    description: "Second meetup",
+  },
+];
 const HomePage = () => {
-  return <div>index</div>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 };
 
 export default HomePage;
--- pages/news/[newsID].js
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router";
-
-const DetailPage = () => {
-  const router = useRouter();
-  const newsId = router.query.newsID;
-  return <div>Detail</div>;
-};
-
-export default DetailPage;
