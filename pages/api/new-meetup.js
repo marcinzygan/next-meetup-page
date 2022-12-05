8b5927314c78678731b308a2d221c78245888b6d
marcinzygan <soul@o2.pl>
2022-12-05 11:27:18 +0100

    added POST method to post meetup into mongodb database , fetching data in getStaticProps

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,9 +1,22 @@
 // api/new-meetup
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+import { useRouter } from "next/router";
+
+async function handler(req, res) {
+  const router = useRouter();
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, description, address } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://souloo:bananek1983@cluster0.pgvopt0.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+    client.close();
+    res.status(201).json({ message: "Meetup added" });
+    router.push("/");
   }
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
 import MeetupList from "../components/meetups/MeetupList";
-
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "first meetup",
-    image:
-      "https://images.unsplash.com/photo-1646905065090-abeff1ccf975?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
-    address: "Portishead, UK",
-    description: "First meetup",
-  },
-  {
-    id: "m2",
-    title: "second meetup",
-    image:
-      "https://images.unsplash.com/photo-1655283658746-7402b3195f88?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
-    address: "Portland, UK",
-    description: "Second meetup",
-  },
-];
+import { MongoClient } from "mongodb";
+// const DUMMY_MEETUPS = [
+//   {
+//     id: "m1",
+//     title: "first meetup",
+//     image:
+//       "https://images.unsplash.com/photo-1646905065090-abeff1ccf975?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+//     address: "Portishead, UK",
+//     description: "First meetup",
+//   },
+//   {
+//     id: "m2",
+//     title: "second meetup",
+//     image:
+//       "https://images.unsplash.com/photo-1655283658746-7402b3195f88?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
+//     address: "Portland, UK",
+//     description: "Second meetup",
+//   },
+// ];
 const HomePage = (props) => {
   return <MeetupList meetups={props.meetups} />;
 };
@@ -30,9 +30,21 @@ const HomePage = (props) => {
 //   };
 // }
 export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://souloo:bananek1983@cluster0.pgvopt0.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+  const meetups = await meetupsCollection.find().toArray();
+  client.close();
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
     // revalidate: 1,
   };
