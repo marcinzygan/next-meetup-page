7beb13dc09537b962d84a7c1a902b94269c067e4
marcinzygan <soul@o2.pl>
2022-12-05 09:08:02 +0100

    added MeetupDEtails component and CSS module

--- components/meetups/MeetupDetails.js
+++ components/meetups/MeetupDetails.js
@@ -1,11 +1,13 @@
+import classes from "./MeetupDetails.module.css";
+
 const MeetupDetails = (props) => {
   return (
-    <>
+    <section className={classes.detail}>
       <img src={props.image} alt={props.title}></img>
       <h1>{props.title}</h1>
       <p>{props.address}</p>
       <p>{props.description}</p>
-    </>
+    </section>
   );
 };
 
